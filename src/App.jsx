1cb07d04c165168721a4ed11bc7919b4c99e6fad
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-05-19 23:53:06 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,11 @@ function App(props) {
 
 
         <Routes>
-            <Route path={"/"} element={<Home/>}/>
+            <Route index element={<Home/>}/>
 
-            {/* catch all, so any unknown pages navigate back to the home page  */}
+
+            {/* catch all, so any unknown pages navigate back to the home page, or
+             error page to show it doesn't exist, then auto redirect home  */}
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     </div>
@@ -46,18 +48,22 @@ npm run build
 
 npm run deploy
 
-
 **To make work with github pages we must do ALL of these **
 
 go into package.json and add
-"homepage": "/github-repo-name/#",
+"homepage": ".",
 
 
 go into vite.config.js and add:
- base: "/github-repo-here"
+ base: "/github-repo-here/"
 
-inside main.jsx -> the router component needs a basename or else it will not show up in the github page link
-<Router basename={'/react-directory-here'}>
+HashRouter is for when we don't have a server handling routes, it uses history api, BrowserRouter is for when we have server
+    BrowserRouter works in local host because its' a server handling it, but fails on Github pages with no server
+<HashRouter>
+        <App colorMode={"light-mode"} />
+ </HashRouter>
+
+<Router basename={'/github-repo-here'}>
     <App />
 </Router>
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {BrowserRouter as Router, HashRouter} from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 import {AppContextProvider} from "./components/AppContextProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
   <AppContextProvider>
-    <HashRouter>
+    <HashRouter >
         <App colorMode={"light-mode"} />
     </HashRouter>
   </AppContextProvider>
