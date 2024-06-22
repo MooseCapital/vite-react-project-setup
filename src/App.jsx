9b6020e1bf424d790fdc8c3cae5a48e0f8565c0b
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2024-06-22 08:50:11 -0500

    Lazy error page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Home from "./components/Home.jsx";
 import {localStore, normalStore} from "./store.js";
 import {CircularProgress} from '@mui/joy';
 
-//do NOT lazy load error pages or we will get errors!
-// const LazyErrorPage404 = lazy(() => import('./components/ErrorPage404.jsx'));
-import ErrorPage404 from "./components/ErrorPage404.jsx";
+//do NOT lazy load error page boundary or we will get errors! we can lazy load the 404 page though
+const LazyErrorPage404 = lazy(() => import('./components/ErrorPage404.jsx'));
+// import ErrorPage404 from "./components/ErrorPage404.jsx";
 import {PageErrorBoundary} from "./components/PageErrorBoundary.jsx";
 const TestPageLazy = lazy(() => import('./components/Test.jsx'));
 const AboutPageLazy = lazy(() => import('./components/About.jsx'));
@@ -17,7 +17,8 @@ const AboutPageLazy = lazy(() => import('./components/About.jsx'));
 
 function App(props) {
 
-
+    //we can remove the localstore here and
+    //change the div className App to <> fragment if we use a css library like MUI
     const {colorMode} = localStore((state) => ({
         colorMode: state.colorMode
     }));
@@ -32,7 +33,7 @@ function App(props) {
                         <Route path="/about" element={<AboutPageLazy/>}/>
 
                         {/* * is for any path that is NOT defined, if the user types it in the search bar, we redirect to 404 error page */}
-                        <Route path="*" element={<ErrorPage404/>}/>
+                        <Route path="*" element={<LazyErrorPage404/>}/>
                     </Route>
                 </Routes>
             </PageErrorBoundary>
